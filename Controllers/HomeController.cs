efeaef693b47865856c6ab37adca1ffa7df5938b
EE <ercanesee@gmail.com>
2022-08-16 10:22:38 +0300

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mercedes.Models;
+using mercedes.Models.Entities;
+
+namespace mercedes.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private NorthwindContext context;
+
+    public HomeController(NorthwindContext _context)
+    {
+        context = _context;
+    }
+
+    public IActionResult Index()
+    {
+        List<Region> list =  context.Regions.ToList();
+ 
+        return View(list);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mercedes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
